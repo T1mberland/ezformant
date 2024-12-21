f55ad389f7b14d1baf8ae91daf43d940bcbd560b
T1mberland <ymokc1120@gmail.com>
2024-12-21 17:59:05 +0900

    ez fft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rustfft::{FftPlanner, num_complex::Complex};
+
+fn basic_fft() {
+    let mut planner = FftPlanner::new();
+    let fft = planner.plan_fft_forward(2048);
+
+    let mut buffer = vec![Complex{ re: 0.0f32, im: 0.0f32}; 2048];
+    for i in 0..2048 {
+        buffer[i] = Complex { re: f32::cos(i as f32), im: 0.0f32 };
+    }
+
+    fft.process(&mut buffer);
+    
+    for i in 0..2048 {
+        println!("{}", buffer[i].re);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    basic_fft();
 }
