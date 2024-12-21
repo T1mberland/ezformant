b912214919f1814a44fa31abde1546ccaa065e02
T1mberland <ymokc1120@gmail.com>
2024-12-21 18:33:39 +0900

    added simple fft in rust for testing

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,18 @@ pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
+#[wasm_bindgen]
+pub fn process_audio(data: Vec<f32>, lpc_order: usize) -> Vec<f32> {
+    let len = data.len();
+    let mut fft_input: Vec<Complex<f32>> = data.iter().map(|&x| Complex::new(x, 0.0)).collect();
+    let mut planner = FftPlanner::new();
+    let fft = planner.plan_fft_forward(len);
+
+    fft.process(&mut fft_input);
+
+    fft_input.iter().map(|x| x.re).collect()
+}
+
 fn basic_fft() {
     let mut planner = FftPlanner::new();
     let fft = planner.plan_fft_forward(2048);
