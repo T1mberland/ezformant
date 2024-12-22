64a23769313d914922e6a8bb3b037b0c5f630723
T1mberland <104497753+T1mberland@users.noreply.github.com>
2024-12-22 10:08:02 +0900

    can't make this thing work

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import init, { process_audio } from './pkg/ezformant.js';
+import init, { process_audio, lpc_filter_freq_responce } from './pkg/ezformant.js';
 
 const canvas = document.getElementById('spectrum');
 const ctx = canvas.getContext('2d');
@@ -15,6 +15,7 @@ async function start() {
     analyser.fftSize = 2048;
     const bufferLength = analyser.fftSize;
     const dataArray = new Float32Array(bufferLength);
+    const filterDataArray = new Float32Array(bufferLength);
     const sampleRate = audioContext.sampleRate;
 
     // Number of bars in the spectrum
@@ -84,7 +85,42 @@ async function start() {
         requestAnimationFrame(drawSpectrum);
     }
 
+    function drawLPCFilter() {
+      analyser.getFloatTimeDomainData(dataArray);
+
+      for (let i = 0; i < dataArray.length; i++) {
+        let window = 0.5 * (1 - Math.cos((2 * Math.PI * i) / (dataArray.length - 1)));
+        dataArray[i] *= window;
+      }
+
+      const freqResponce = lpc_filter_freq_responce(Array.from(dataArray), 8, sampleRate, numBars);
+
+      if (freqResponce.every(value => value === 0)) {
+          requestAnimationFrame(drawLPCFilter);
+          return;
+      }
+
+      ctx.beginPath();
+      ctx.moveTo(0,0);
+      for (let i = 0; i < numBars; ++i) {
+        const freqStart = frequencyBins[i];
+
+        const binStart = Math.floor(freqStart * analyser.fftSize / sampleRate);
+
+        const xPos = canvas.width * freqStart / maxFrequency;
+        const yPos = freqResponce[freqStart];
+
+        ctx.lineTo(xPos, yPos);
+        ctx.moveTo(xPos, yPos);
+      }
+      ctx.stroke();
+      //console.log(freqResponce);
+
+      requestAnimationFrame(drawLPCFilter);
+    }
+
     drawSpectrum();
+    drawLPCFilter();
 }
 
 start();
