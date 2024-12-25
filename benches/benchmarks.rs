ddd212d87fe1c436f8af47d6a2e972d237cf135d
T1mberland <104497753+T1mberland@users.noreply.github.com>
2024-12-25 19:27:08 +0900

    levinson, tested

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,15 +1,8 @@
-use serde_json::{Result, Value, Number};
 use serde::Deserialize;
-use std::{f32::EPSILON, fs};
+use std::fs;
 use std::path::PathBuf;
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::collections::HashMap;
-use ezformant::*;
-
-use rustfft::{num_complex::{Complex, ComplexFloat}, FftPlanner};
-use wasm_bindgen::prelude::*;
-use aberth::aberth;
-use aberth::AberthSolver;
 
 const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
 
@@ -44,8 +37,14 @@ const FIXED_LPC_ORDER: usize = 14;
 fn criterion_bench(c: &mut Criterion) {
     let data = read_audio_data();
     const FRAME_LENGTH: usize = 2048;
+
+    let mut downsampled: [f64; FRAME_LENGTH] = [0.0; FRAME_LENGTH];
+    downsampler(&data, &mut downsampled, 4);
+    let r = autocorrelate(&downsampled, FIXED_LPC_ORDER);
+
     
     // Solver bench
+    /*
     c.bench_function("bench test", |b| b.iter(|| {
         // Test 1
         //formant_detection(data.clone(), FIXED_LPC_ORDER, 441000f64);
@@ -54,23 +53,14 @@ fn criterion_bench(c: &mut Criterion) {
         // Tets2: no change
         formant_detection_fixed(data.clone(), 441000.0f64);
     }));
-
-    /*
-    c.bench_function("bench test", |b| b.iter(|| {
-        let mut downsampled: [f64; FRAME_LENGTH];
-        let mut tmp: [f64; FIXED_LPC_ORDER+1] = [0.0;FIXED_LPC_ORDER+1];
-
-        downsampler(&data, &mut downsampled, 4);
-
-        let coeffs = lpc::levinson();
-
-        for i in 0..=FIXED_LPC_ORDER {
-            tmp[i] = data[i];
-        }
-
-        formant_detection_fixed(&tmp, 441000.0f64);
-    }));
     */
+
+    c.bench_function("levinson, test", |b| b.iter(|| {
+
+        //levinson(FIXED_LPC_ORDER, &r);
+        levinson2(FIXED_LPC_ORDER, &r); // very fast
+
+    }));
 }
 
 fn pre_emphasis(signal: &mut [f64], alpha: f64) {
@@ -114,7 +104,6 @@ fn autocorrelate(signal: &[f64], maxlag: usize) -> Vec<f64> {
 /// 
 /// # Arguments
 /// 
-/// * `signal` - A slice of f64 representing the input signal.
 /// * `order` - The order of the recursion.
 /// * `r` - An optional slice of f64 representing the autocorrelation coefficients.
 /// 
@@ -123,7 +112,7 @@ fn autocorrelate(signal: &[f64], maxlag: usize) -> Vec<f64> {
 /// A tuple containing:
 /// - A vector of filter coefficients (`a`).
 /// - The final prediction error (`E`).
-fn levinson(signal: &[f64], order: usize, r: &[f64]) -> (Vec<f64>, f64){
+fn levinson(order: usize, r: &[f64]) -> (Vec<f64>, f64){
     let p = order;
 
     if p == 0 {
@@ -134,7 +123,7 @@ fn levinson(signal: &[f64], order: usize, r: &[f64]) -> (Vec<f64>, f64){
     }
 
 
-    let (aa, ee) = levinson(signal, p-1, r);
+    let (aa, ee) = levinson(p-1, r);
 
     let mut k = 0.0;
     for j in 0..p {
@@ -188,81 +177,6 @@ fn levinson2(order: usize, r: &[f64]) -> (Vec<f64>, f64){
     }
 
     (a, e)
-}
-
-
-fn peak_detection_fixed<const N:usize>(lpc_coeffs: &[f64; N], sample_rate: f64) -> Vec<f64> {
-    const EPSILON: f64 = 0.001;
-    const MAX_ITERATIONS: u32 = 15;
-    let mut solver = AberthSolver::new();
-    solver.epsilon = EPSILON;
-    solver.max_iterations = MAX_ITERATIONS;
-
-    let roots = aberth(&lpc_coeffs, MAX_ITERATIONS, EPSILON).to_vec();
-    let mut peaks = Vec::with_capacity(roots.len());
-
-    for root in roots {
-        let theta = root.arg();
-        if 0.0 <= theta {
-            peaks.push(theta * sample_rate / 2.0 / std::f64::consts::PI);
-        } else if - std::f64::consts::PI <= theta && theta < 0.0 {
-            peaks.push((theta + 2.0 * std::f64::consts::PI) * sample_rate / 2.0 / std::f64::consts::PI);
-        } else {
-            // Won't happen
-        }
-    }
-
-    peaks
-}
-
-fn _formant_detection_fixed<const N:usize>(lpc_coeffs: &[f64; N], sample_rate: f64) -> Vec<f64> {
-    let peaks = peak_detection_fixed(lpc_coeffs, sample_rate);
-    let mut formants = Vec::with_capacity(peaks.len());
-
-    for peak in peaks {
-        if peak < 10.0 || peak > (sample_rate/2.0 - 10.0) { continue; }
-
-        formants.push(peak);
-    }
-
-    formants.sort_by(|a, b| a.partial_cmp(b).unwrap());
-
-    formants
-}
-
-
-fn formant_detection_fixed(
-    mut data: Vec<f64>, 
-    sample_rate: f64,
-) -> Vec<f64> {
-    const FORMANT_NUM: usize = 4;
-
-    // Subtract the mean to make the signal zero-mean
-    let mean = data.iter().copied().sum::<f64>() / data.len() as f64;
-    for sample in data.iter_mut() {
-        *sample -= mean;
-    }
-
-    // Apply windowing (e.g., Hamming window)
-    for i in 0..data.len() {
-        data[i] *= 0.54 - 0.46 * (2.0 * std::f64::consts::PI * i as f64 / (data.len() as f64 - 1.0)).cos();
-    }
-
-    pre_emphasis(&mut data, 0.97);
-
-    let r = autocorrelate(&data, FIXED_LPC_ORDER);
-    let (lpc_coeff, _) = levinson(&data, FIXED_LPC_ORDER, &r);
-    
-    let mut lpc_coeff_slice: [f64; FIXED_LPC_ORDER+1] = [0.0; FIXED_LPC_ORDER+1];
-
-    for i in 0..=FIXED_LPC_ORDER {
-        lpc_coeff_slice[i] = lpc_coeff[i];
-    }
-
-    let formants = _formant_detection_fixed(&lpc_coeff_slice, sample_rate);
-
-    formants
-
 }
 
 criterion_group!(benches, criterion_bench);
